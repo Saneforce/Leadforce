910d3518f51f0b0f248c050a13d698cce68636ed
Rajeshkumar <rajeshkumar.r@techmango.net>
2022-07-08 08:57:52 +0530

    report

--- application/views/admin/reports/deals.php
+++ application/views/admin/reports/deals.php
@@ -1,158 +1,168 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <?php init_head(); ?>
 <div id="wrapper">
-	<div class="content"> 
-		<div class="row">
-		<?php if ($this->session->flashdata('debug')) {?>
-			<div class="col-lg-12">
-				<div class="alert alert-warning">
-					<?php echo $this->session->flashdata('debug'); ?>
-				</div>
-			</div>
-		<?php
-		} 
-		$record_val = end($this->uri->segment_array());
-		?>
-		<div class="modal fade" id="clientid_add_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" data-keyboard="false" data-backdrop="static">
-			<div class="modal-dialog" role="document">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button group="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title" id="myModalLabel">
-							<span class="edit-title"><?php echo _l('add_report'); ?></span>
-						</h4>
-					</div>
-					<?php echo form_open('admin/reports/save_report',array('id'=>'clientid_add_group_modal')); ?>
-					<div class="modal-body">
-						<input type="hidden" id="cur_id12" value="<?php echo $id;?>">
-							<?php $attrs = array('autofocus'=>true, 'required'=>true); ?>
-							<?php echo render_input( 'name', 'name','','text',$attrs); ?>
-							<div id="companyname_exists_info" class="hide"></div>
-							<div class="form-group select-placeholder contactsdiv" >
-								<label for="project_contacts_selectpicker"
-                                class="control-label"><small class="req text-danger">* </small><?php echo _l('folder'); ?></label>
-								 <div class="input-group input-group-select ">
-								 <?php 
-								 $selected = '';
-								 echo render_select('folder',$folders,array('id',array('folder')),false,$selected,array('aria-describedby'=>'project_contacts-error','style'=>'height:21px;'),array(),'cur_class','',false);?>
-								 <div class="input-group-addon" style="opacity: 1;">
-									<a href="#" data-toggle="modal" data-target="#section_modal" ><i class="fa fa-plus"></i></a>
-									</div>
-								</div>
-							</div>
-					</div>
-					<div class="modal-footer">
-						<button group="button" class="btn btn-default" data-dismiss="modal"><?php echo _l('close'); ?></button>
-						<button group="submit" class="btn btn-info"><?php echo _l('submit'); ?></button>
-						
-					</div>
-					<?php echo form_close(); ?>
-				</div>
-			</div>
-		</div>
-		<div class="modal fade" id="section_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" data-keyboard="false" data-backdrop="static">
-			<div class="modal-dialog" role="document">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button group="button" class="close" data-dismiss="modal" aria-label="Close"><span
-								aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title" id="myModalLabel">
-							<span class="edit-title"><?php echo _l('add_new',_l('folder')); ?></span>
-						</h4>
-					</div>
-					<?php echo form_open('admin/reports/add_folder',array('id'=>'section_add')); ?>
-					<div class="modal-body">
-						<div class="row">
-							<div class="col-md-12">
-								<?php $attrs = array('autofocus'=>true, 'required'=>true); ?>
-								<?php echo render_input( 'name', 'name','','',$attrs); ?>
-								<div id="contact_exists_info" class="hide"></div>								
-								<div class="input_fields_wrap_ae">
-								
-								</div>
-								
-							</div>
-						</div>
-					</div>
-					<div class="modal-footer">
-						<button group="button" class="btn btn-default" data-dismiss="modal"><?php echo _l('close'); ?></button>
-						<button group="submit" class="btn btn-info"><?php echo _l('submit'); ?></button>
+   <div class="content">
+      <div class="row">
+         <div class="col-md-12">
+            <div class="panel_s">
+               <div class="panel-body">
+                  <div class="row">
+                     <div class="col-md-4 border-right">
+                      <h4 class="no-margin font-medium"><i class="fa fa-usd" aria-hidden="true"></i> <?php echo _l('deals_report_heading'); ?></h4>
+                      <hr />
+                      <p>
+                        <a href="#" class="font-medium" onclick="init_report(this,'deals-won-report'); return false;"><i class="fa fa-caret-down" aria-hidden="true"></i> <?php echo _l('Deals won'); ?></a>
+                      </p>
+                        <hr class="hr-10" />
+                     <p>
+                        <a href="#" class="font-medium" onclick="init_report(this,'deals-Lost-report'); return false;"><i class="fa fa-caret-down" aria-hidden="true"></i> <?php echo _l('Deals Lost'); ?></a>
+                      </p>
+                      <hr class="hr-10" />
+                         <p>
+                        <a href="#" class="font-medium" onclick="init_report(this,'deals-started-report'); return false;"><i class="fa fa-caret-down" aria-hidden="true"></i> <?php echo _l('Deals Started'); ?></a>
+                      </p>
+                      <hr class="hr-10" />
+                      
+                  </div>
+                  <div class="col-md-4 border-right">
+                    <?php
+                    $assigned_attrs = array('data-none-selected-text'=>'All');
+                    $tm = array("id" => "", "name" => "All");
+                    array_unshift($pipelines, $tm);
+                    echo render_select('pipeline_id', $pipelines, array('id', 'name'), 'pipeline', '', $assigned_attrs);
+                    ?>
+				  <?php 
+				  if(count($teammembers) > 1) {
+						  array_unshift($teammembers, $tm);
+				  }
+				  if(!empty($need_fields) && in_array("teamleader", $need_fields) ){
+					echo render_select('teamleader', $teammembers, array('id', 'name'), 'teamleader', '', $assigned_attrs);
+				  }
+					?>
+                 </div>
+                 <div class="col-md-4">
+                      <div class="bg-light-gray border-radius-4">
+                        <div class="p8">
+                             <?php if(isset($currencies)){ ?>
+                  <div id="currency" class="form-group hide">
+                     <label for="currency"><i class="fa fa-question-circle" data-toggle="tooltip" title="<?php echo _l('report_deals_base_currency_select_explanation'); ?>"></i> <?php echo _l('currency'); ?></label><br />
+                     <select class="selectpicker" name="currency" data-width="100%" data-none-selected-text="<?php echo _l('dropdown_non_selected_tex'); ?>">
+                        <?php foreach($currencies as $currency){
+                           $selected = '';
+                           if($currency['isdefault'] == 1){
+                              $selected = 'selected';
+                           }
+                           ?>
+                           <option value="<?php echo $currency['id']; ?>" <?php echo $selected; ?>><?php echo $currency['name']; ?></option>
+                           <?php } ?>
+                        </select>
+                     </div>
+                     <?php } ?>
+                     <div id="income-years" class="hide mbot15">
+                        <label for="payments_years"><?php echo _l('year'); ?></label><br />
+                        <select class="selectpicker" name="payments_years" data-width="100%" onchange="total_income_bar_report();" data-none-selected-text="<?php echo _l('dropdown_non_selected_tex'); ?>">
+                           <?php foreach($payments_years as $year) { ?>
+                           <option value="<?php echo $year['year']; ?>"<?php if($year['year'] == date('Y')){echo 'selected';} ?>>
+                              <?php echo $year['year']; ?>
+                           </option>
+                           <?php } ?>
+                        </select>
+                     </div>
+                     <div class="form-group hide" id="report-time">
+                        <label for="months-report"><?php echo _l('period_datepicker'); ?></label><br />
+                        <select class="selectpicker" name="months-report" data-width="100%" data-none-selected-text="<?php echo _l('dropdown_non_selected_tex'); ?>">
+                           <option value=""><?php echo _l('report_sales_months_all_time'); ?></option>
+                           <option value=""><?php echo _l('report_sales_months_all_time'); ?></option>
+                           <option value="today"><?php echo _l('Today'); ?></option>
+                           <option value="yesterday"><?php echo _l('Yesterday'); ?></option>
+                           <option value="this_week"><?php echo _l('This Week'); ?></option>
+                           <option value="last_week"><?php echo _l('Last Week'); ?></option>
+                           <option value="this_month"><?php echo _l('this_month'); ?></option>
+                           <option value="1"><?php echo _l('last_month'); ?></option>
+                           <option value="this_year"><?php echo _l('this_year'); ?></option>
+                           <option value="last_year"><?php echo _l('last_year'); ?></option>
+                           <option value="3" data-subtext="<?php echo _d(date('Y-m-01', strtotime("-2 MONTH"))); ?> - <?php echo _d(date('Y-m-t')); ?>"><?php echo _l('report_sales_months_three_months'); ?></option>
+                           <option value="6" data-subtext="<?php echo _d(date('Y-m-01', strtotime("-5 MONTH"))); ?> - <?php echo _d(date('Y-m-t')); ?>"><?php echo _l('report_sales_months_six_months'); ?></option>
+                           <option value="12" data-subtext="<?php echo _d(date('Y-m-01', strtotime("-11 MONTH"))); ?> - <?php echo _d(date('Y-m-t')); ?>"><?php echo _l('report_sales_months_twelve_months'); ?></option>
+                           <option value="custom"><?php echo _l('period_datepicker'); ?></option>
+                        </select>
+                     </div>
+                     <div id="date-range" class="hide mbot15">
+                        <div class="row">
+                           <div class="col-md-6">
+                              <label for="report-from" class="control-label"><?php echo _l('report_sales_from_date'); ?></label>
+                              <div class="input-group date">
+                                 <input type="text" class="form-control datepicker" id="report-from" name="report-from">
+                                 <div class="input-group-addon">
+                                    <i class="fa fa-calendar calendar-icon"></i>
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="col-md-6">
+                              <label for="report-to" class="control-label"><?php echo _l('report_sales_to_date'); ?></label>
+                              <div class="input-group date">
+                                 <input type="text"  class="form-control datepicker" disabled="disabled" id="report-to" name="report-to">
+                                 <div class="input-group-addon">
+                                    <i class="fa fa-calendar calendar-icon"></i>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                        </div>
+                      </div>
+                  </div>
+               </div>
+               <div id="report" class="hide">
+               <hr class="hr-panel-heading" />
+               <h4 class="no-mtop"><span id="heading_dynamic"></span> <span><?php echo _l('reports_deals_generated_report'); ?></span>
 
-					</div>
-					<?php echo form_close(); ?>
-				</div>
-			</div>
-		</div>
-		<div class="panel_s project-menu-panel" style="margin-bottom:0px;">
-			<div class="panel-body">
-				<div class="horizontal-tabs">
-					<div class="" style="float:left">
-						 <span id="ch_1_filter1"><?php echo count($filters);?></span> filters applied
-						 <input type="hidden" id="c_filter_val1" value="<?php echo count($filters);?>"> 
-					</div>
-					<?php if(empty($id)){?>
-						<div class="float-right" style="float:right">
-							<button type="button" class="btn btn-primary pull-right1" style="background-color:#61c786 !important;" data-toggle="modal" data-target="#clientid_add_modal">Save</button>
-						</div>
-					<?php }else{?>
-						<div class="float-right" style="float:right">
-							<button type="button" class="btn btn-primary pull-right1" style="background-color:#61c786 !important;" data-toggle="modal" data-target="#public_add_modal">Public Link</button>
-							<a href="<?php echo admin_url('reports/update_report/'.$id);?>" class="btn btn-primary pull-right1" style="background-color:#61c786 !important;" >Save</a>
-							<button type="button" class="btn btn-primary pull-right1" style="background-color:#61c786 !important;" data-toggle="modal" data-target="#clientid_add_modal">Save New</button>
-						</div>
-					<?php }?>
-				</div>
-			</div>
-		</div>
-		<?php if(!empty($id)){?>
-			<div class="modal fade" id="public_add_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" data-keyboard="false" data-backdrop="static">
-				<div class="modal-dialog" role="document">
-					<div class="modal-content">
-						<div class="modal-header">
-							<button group="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-							<h4 class="modal-title" id="myModalLabel">
-								<span class="edit-title"><?php echo _l('public_link'); ?></span>
-							</h4>
-						</div>
-						<div class="modal-body">
-							<div>
-								Create separate links to control access for different viewer groups and name each link accordingly. To revoke access, simply delete a link.
-							</div>
-							<div id="public_all">
-								<?php if(!empty($links)){
-									foreach($links as $link12){
-									?>
-										<div class="form-group" app-field-wrapper="name" ><label for="name" class="control-label"> Share Link</label><input type="text"  class="form-control" value="<?php echo base_url('shared/index/'.$link12['share_link']);?>"  readonly style="width:90%;float:left;"><a href="javascript:void(0);" " style="margin-left:10px;float:left" onclick="delete_link('<?php echo $link12['id'];?>')"><i class="fa fa-trash fa-2x" style="color:red"></i></a></div>
-								<?php 
-									}
-								}?>
-							
-							</div>
-							<div class="row"> <div class="col-md-12"><a href="javascript:void(0)" onclick="add_public_link('<?php echo $id;?>')">Add Link</a></div></div>
-						</div>
-						<div class="modal-footer">
-							<button group="button" class="btn btn-default" data-dismiss="modal"><?php echo _l('close'); ?></button>
-						</div>
-					</div>
-				</div>
-			</div>
-		<?php }?>
-		<?php echo $report_filter;?>
-		<div class="panel_s project-menu-panel">
-			<div class="panel-body">
-				<div class="col-md-12">
-					<div id="overlay_deal" style="display: none;"><div class="spinner"></div></div>
-<?php $this->load->view('admin/reports/deal_list_column'); ?>
-					<?php //echo $tab_view;?>
-								<?php $this->load->view('admin/reports/deal_table_html'); ?>
-				</div>
-				<div class="clearfix"></div>
-			</div>
-		</div>
-	</div>
-	<div class="btn-bottom-pusher"></div>
-	</div>
-	<input type="hidden" id="check_search_id">
+                     <div class="form-group pull-right hide" id="deals-loss-by-div" style="margin: -10px auto 0px auto; width: 20%;">
+                        <select class="selectpicker" name="deals-loss-by" data-width="100%" data-none-selected-text="<?php echo _l('dropdown_non_selected_tex'); ?>">
+                        <option></option>   
+                        <option value="name"><?php echo _l('By Users'); ?></option>
+                           <option value="status"><?php echo _l('By Stages'); ?></option>
+                           <option value="loss_reason"><?php echo _l('By Reasons'); ?></option>
+                        </select>
+                     </div>
+
+                     <div class="form-group pull-right hide" id="deals-started-by-div" style="margin: -10px auto 0px auto; width: 20%;">
+                        <select class="selectpicker" name="deals-started-by" data-width="100%" data-none-selected-text="<?php echo _l('dropdown_non_selected_tex'); ?>">
+                        <option></option>     
+                        <option value="name"><?php echo _l('By Users'); ?></option>
+                           <option value="status"><?php echo _l('By Current Status'); ?></option>
+                        </select>
+                     </div>
+               
+               </h4>
+               <hr class="hr-panel-heading" />
+               <?php $this->load->view('admin/reports/includes/deals_won'); ?>
+               <?php $this->load->view('admin/reports/includes/deals_loss'); ?>
+               <?php $this->load->view('admin/reports/includes/deals_started'); ?>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
 </div>
-<?php init_tail(); app_admin_ajax_search_function();?>
-<?php echo $report_footer;?>
+</div>
+<?php init_tail(); ?>
+<?php $this->load->view('admin/reports/includes/deals_js'); ?>
+<style>
+.dropdown.open .deals_wons_details{
+   margin: 0px auto 0px auto !important;
+}
+.divdwdr,.divdldr,.divdsdr{
+   position: absolute !important;
+   width: 90%;
+   margin-left: -15%;
+   overflow-y: auto;
+   z-index: 1;
+}
+.divdldr{
+   margin-left: -10%;
+}
+</style>
+
+</body>
+</html>
